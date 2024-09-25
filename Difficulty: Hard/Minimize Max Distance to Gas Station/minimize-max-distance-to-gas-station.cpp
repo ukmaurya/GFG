7d706f4ea59cb17d36441f7721ab39b69efb9c41
ukmaurya <ukmaurya30@gmail.com>
2024-09-25 10:01:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.cpp
+++ Difficulty: Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.cpp
@@ -5,51 +5,63 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-    private:
-   bool isPossible(double mid , vector<int>&stations , int k ){
+    bool possible(vector<int>&stations , double mid , int k ){
         int n = stations.size();
-        int count=0;
-        for(int i=0;i<n-1;i++){
-           int diff = stations[i+1] - stations[i];
-           int num = diff/mid;
-           if(num == (stations[i+1] - stations[i])/mid){
-               num = num-1;
-           }
-           count = count+num;
-            
+        int atleastStations = 0;
+        for(int i=1;i<n;i++){
+            double diff = stations[i]-stations[i-1];
+            atleastStations += floor(diff/mid*1.0);
         }
-        if(count<=k){
+        if(atleastStations<=k){
             return true;
-            
         }
         return false;
     }
   public:
     double findSmallestMaxDist(vector<int> &stations, int k) {
-          
-          
-         int maxi =INT_MIN;
+         
+        //  priority_queue<double> pq;
+        //  int n = stations.size();
+        //  for(int i=1;i<n;i++){
+        //      pq.push(stations[i]-stations[i-1]);
+        //  }
+       
+        //  while(k--){
+        //      double t = pq.top();
+        //      pq.pop();
+        //      pq.push(t/2);
+        //      pq.push(t/2);
+           
+             
+        //  }
+        //  double ans = INT_MIN;
+        //  while(!pq.empty()){
+        //      ans = max(ans , pq.top());
+        //      pq.pop();
+        //  }
+        //  return ans;
+        
+        // above method is wrong check example -- distance is 1 to 12 and u want to put 2 more station 
+   
+         int maxi = 0;
          int n = stations.size();
-         for(int i=0;i<n-1;i++ ){
-            maxi = max(maxi , stations[i+1]-stations[i]);
+         for(int i=0;i<n-1;i++){
+              maxi = max(maxi , stations[i+1]-stations[i]);
          }
          double start = 0;
-         double end = maxi ;
-         double ans=-1;
-          while(end-start > 1e-6){
-             double mid = start + (end-start)/2.0;
-              if(isPossible(mid , stations , k)){
-                 
-                 end = mid;
-                  ans = mid;
-              }
-              else{
-                 
-                   start = mid;
-              }
-          }
-          
-          return ans;
+         double end = maxi;
+         double ans =-1;
+         while(end-start > 1e-6){
+             double mid = start-(start-end)/2.0;
+             if(possible(stations , mid ,k)){
+                 ans = mid;
+                 end = mid; // since double 
+             }
+             else{
+                 start = mid;
+             }
+         }
+       return ans;
     }
 };
 
