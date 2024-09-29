5c42a2f394fc3031632b3f62131f76b8045a4d51
ukmaurya <ukmaurya30@gmail.com>
2024-09-29 17:25:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Job Sequencing Problem/job-sequencing-problem.cpp
+++ Difficulty: Medium/Job Sequencing Problem/job-sequencing-problem.cpp
@@ -22,109 +22,62 @@ struct Job
     int profit; // Profit if job is over before or on deadline 
 };
 */
+
 class disjointset{
-    public: 
-    vector<int> size , parent;
-    
-    disjointset(int n){
-        size.resize(n+1,1);
-        parent.resize(n+1);
-        for(int i=0;i<=n;i++){
-            parent[i]=i;
-        }
-    }
-    
-    int findulp(int u){
-         if(parent[u]==u){
-             return u;
-         }
-         
-       return parent[u] = findulp(parent[u]);
-    }
-    
-    void unionSize(int u , int v){
-        
-        //litle change
-    
-        
-       int x = findulp(u);
-        int y = findulp(v);
-        if(x<y){
-            parent[y]=x;  // connect to smaller slot =>smaller slot will be available
-        }
-        else{
-            parent[x]=y;
-        }
-     
-        /*if(x!=y){
-           if(size[x] <= size[y]){
-               parent[x]=parent[y];
-               size[y] += size[x];
-           }
-           else{
-               parent[y]=parent[x];
-               size[x] += size[y];
-           }
-        } */
-        return; 
-       
-       
-    }
+      vector<int> parent , size;
+      public: 
+      disjointset(int n ){
+          parent.resize(n+1);
+          for(int i=0;i<=n;i++){
+              parent[i]=i;
+          }
+          size.resize(n+1);
+          
+      }
+      
+      int ultiPar(int node){
+          if(parent[node]==node){
+              return node;
+          }
+          return parent[node] = ultiPar(parent[node]);
+      }
+      
+      void unionSize(int u , int v){
+          parent[v]=u;
+      }
 };
+
 
 class Solution 
 {
-    bool static comp(Job j1 , Job j2){
-        return j1.profit > j2.profit;
-        
+    bool static comp(Job arr1 , Job arr2){
+        return arr1.profit > arr2.profit;
     }
     public:
     //Function to find the maximum profit and the number of jobs done.
     vector<int> JobScheduling(Job arr[], int n) 
     { 
-        /*    sort(arr, arr+n,comp);   taking o(n^2) time in worst case
-            int profit = 0;
-            int count= 0;
-            vector<int> sch(n,-1); // initially no jobs are scheduled
-            for(int i=0;i<n;i++){
-                int j = arr[i].dead-1;// 0 based indexing
-                while(j>=0 && sch[j] != -1){
-                    j--;
-                }
-                if(j>=0){
-                    profit += arr[i].profit;
-                    count++;
-                    sch[j]=i;
-                }
-               
-            }
-            
-            
-            return {count , profit}; */
-            
-          // we will use disjointset o(nlogn) time
-          
-          sort(arr, arr+n,comp);
-          
-        
-          int maxDeadline = 0;
+          sort(arr, arr+n , comp);
+          int maxi =0;
           for(int i=0;i<n;i++){
-             maxDeadline = max(maxDeadline , arr[i].dead);
+              maxi = max(maxi , arr[i].dead);
           }
+          disjointset ds(maxi);
           
-          disjointset ds(maxDeadline);
+          int ans =0;
           int count=0;
-          int prof =0;
           for(int i=0;i<n;i++){
-              int slot = ds.findulp(arr[i].dead);
-              if(slot!=0){
+              int deadline = arr[i].dead;
+              int faiyda = arr[i].profit;
+              int par = ds.ultiPar(deadline);
+              if(par>0){
+                  ds.unionSize(par-1,par);
+                  ans+= faiyda;
                   count++;
-                  prof+=arr[i].profit;
-                  ds.unionSize(slot, slot-1);
               }
           }
-            
-          return {count , prof};   
+          
+          return {count ,ans};
     } 
 };
 
