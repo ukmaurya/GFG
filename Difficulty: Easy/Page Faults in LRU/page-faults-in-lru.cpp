f0dfcbd6a92c0f9775757d21fd4d7e1091d06dd8
ukmaurya <ukmaurya30@gmail.com>
2024-09-30 21:40:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Page Faults in LRU/page-faults-in-lru.cpp
+++ Difficulty: Easy/Page Faults in LRU/page-faults-in-lru.cpp
@@ -10,41 +10,40 @@ using namespace std;
 // User function Template for C++
 
 class Solution{
-    
 public:
-    int pageFaults(int n, int C, int pages[]){
+    int pageFaults(int N, int C, int pages[]){
         // code here
-        int timer = 0;
-        unordered_map<int, int> mp1; // page -> time
-        set<pair<int, int>> s; // {time, page} to maintain order of access
-        int pagefaults = 0;
-
-        for (int i = 0; i < n; i++) {
-            int page = pages[i];
-            
-            // If the page is already in cache, update its time
-            if (mp1.find(page) != mp1.end()) {
-                s.erase({mp1[page], page}); // Remove the old time entry
-            } else {
-                // Page fault occurs
-                pagefaults++;
-                
-                // If cache is full, remove the least recently used page
-                if (mp1.size() == C) {
-                    auto lru = s.begin(); // Get the least recently used page
-                    mp1.erase(lru->second); // Remove it from the cache
-                    s.erase(lru); // Remove it from the set
-                }
+        int timer=1;
+        unordered_map<int, int> mp; // pageno , timer;
+        set<pair<int, int>>st; // timer , page ;
+        int size = 0;
+        int ans =0;
+        for(int i=0;i<N;i++){
+            if(mp.find(pages[i]) != mp.end()){
+                 int oldTime = mp[pages[i]];
+                 mp[pages[i]]=timer;
+                 st.erase({oldTime, pages[i]});
+                 st.insert({timer , pages[i]});
+                 timer++;
             }
-            
-            // Insert the page with the new time
-            mp1[page] = timer;
-            s.insert({timer, page});
-            timer++;
+            else{
+               if(size<C){
+                   mp[pages[i]]=timer;
+                   st.insert({timer , pages[i]});
+                   size++;
+               }
+               else{
+                   auto lru = st.begin();
+                   mp.erase(lru->second);
+                   st.erase({lru->first , lru->second});
+                   st.insert({timer,pages[i]});
+                   mp[pages[i]]=timer;
+               }
+               timer++;
+               ans++;
+            }
         }
-        
-        return pagefaults;
-        
+        return ans;
     }
 };
 
