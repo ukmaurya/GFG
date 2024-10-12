aa65ebb6f3e703e61120a43709369b8c4daaa239
ukmaurya <ukmaurya30@gmail.com>
2024-10-12 09:55:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
@@ -9,32 +9,52 @@ using namespace std;
 
 class Solution{
     private:
-    int solve(int w , int n , int val[] , int wt[],vector<int> &dp){
+    int solve(int N ,int W , int val[] ,int wt[],vector<vector<int>> &dp){
+        if(W == 0 ) return 0;
+        if(N==0){
+            if( W >= wt[0] ){
+                int temp = W/wt[0];
+                return temp*val[0];
+            }
+            else return 0;
+        }
+        if(dp[N][W] !=-1) return dp[N][W];
+        int ex= solve(N-1 ,W,val,wt,dp );
+        int in = 0;
+        if(W >= wt[N])
+           in = solve(N, W-wt[N] ,val,wt,dp) + val[N];
+           
+         return dp[N][W]=max(ex, in);  
         
-        if(w <= 0){
-            return 0;
-        }
-        if(dp[w]!=-1)
-          return dp[w];
-        int ans = 0;
-        for(int i=0;i<n;i++){
-              if(wt[i]<=w)
-                ans = max(ans , val[i]+solve(w-wt[i] , n , val, wt,dp));
-              else 
-                ans = max(ans , solve(w-wt[i] , n , val, wt,dp));
-             
-            
-        }
-        return dp[w]=ans;
     }
 public:
     int knapSack(int N, int W, int val[], int wt[])
-    {
-        // unbounded knapsack 
-        vector<int> dp(W+1 , -1);
-        return solve(W , N , val , wt,dp);
+    {   
+        // vector<vector<int>> dp(N,vector<int>(W+1,-1));
+        // return solve(N-1, W, val , wt ,dp);
+         vector<vector<int>> dp(N+1,vector<int>(W+1,0));
+         for(int i=0;i<=W;i++){
+            if( i >= wt[0] ){
+                int temp = i/wt[0];
+                dp[0][i] = temp*val[0];
+            } 
+         }
+         
+         for(int i=1;i<N;i++){
+             for(int j=0;j<=W;j++){
+                 int ex = dp[i-1][j];
+                 int in=0;
+                 if(wt[i]<=j){
+                     in = dp[i][j-wt[i]]+val[i];
+                 }
+                 dp[i][j] = max(in,ex);
+             }
+         }
+        
+        return dp[N-1][W];
     }
 };
+
 
 //{ Driver Code Starts.
 
