2ec22face34d68b36a2170455d1a9b76776cbf8c
ukmaurya <ukmaurya30@gmail.com>
2024-10-16 18:59:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Optimal Strategy For A Game/optimal-strategy-for-a-game.cpp
+++ Difficulty: Medium/Optimal Strategy For A Game/optimal-strategy-for-a-game.cpp
@@ -7,36 +7,67 @@ using namespace std;
 //Function to find the maximum possible amount of money we can win.
 #define ll long long 
 class Solution{
-    ll solve(int i , int j , int arr[] , bool turn,vector<vector<ll>>&dp){
+//     ll solve(int i , int j , int arr[] , bool turn,vector<vector<ll>>&dp){
      
-       if(i>j)
-        return 0;
-        if(dp[i][j]!=-1)
-         return dp[i][j];
+//       if(i>j)
+//         return 0;
+//         if(dp[i][j]!=-1)
+//          return dp[i][j];
     
-      ll ans = 0;
+//       ll ans = 0;
     
-       if(turn==true){
-         ll first = solve(i+1 , j,arr , false,dp)+arr[i];
-         ll sec = solve(i,j-1,arr,false,dp)+arr[j];
-           ans = max(first , sec);
-       }
-       else{
-         ll first = solve(i+1 , j,arr , true,dp);
-         ll sec = solve(i,j-1,arr,true,dp );
-          ans = min(first , sec);
-       }
-     return dp[i][j]=ans ;
+//       if(turn==true){
+//          ll first = solve(i+1 , j,arr , false,dp)+arr[i];
+//          ll sec = solve(i,j-1,arr,false,dp)+arr[j];
+//           ans = max(first , sec);
+//       }
+//       else{
+//          ll first = solve(i+1 , j,arr , true,dp);
+//          ll sec = solve(i,j-1,arr,true,dp );
+//           ans = min(first , sec);
+//       }
+//      return dp[i][j]=ans ;
      
-}
+// }
     public:
     long long maximumAmount(int n, int arr[]){
         // Your code here
-         vector<vector<ll>>dp(n,vector<ll>(n,-1));
-         return solve(0,n-1,arr,true,dp);
+        //  vector<vector<ll>>dp(n,vector<ll>(n,-1));
+        //  return solve(0,n-1,arr,true,dp);
+        
+        
+        // above is by using the tur variable 
+        
+        
+        //approach 2 : without using the turn variable 
+        
+        // concept is same , we try to maximise our score, while opponent want to 
+         // maximise theor score means minimise our score
+        vector<vector<ll>>dp(n,vector<ll>(n,-1));
+        return solve(0,n-1,n, arr,dp);   
+      }
+    int solve(int i , int j , int n  ,int arr[] ,vector<vector<ll>>&dp ){
+        if(i==j){
+            return arr[i];
+        }
+        if(i>j){
+            return 0;
+        }
+          if(dp[i][j]!=-1)
+           return dp[i][j];
+        
+        // aur apponents wants to decrease our score 
+        int appo1 = min( solve(i+2 ,j , n , arr,dp )  , solve(i+1 , j-1 , n , arr,dp)); // a[i] chosen by us 
+        int appo2 = min(solve(i,j-2,n,arr,dp) , solve(i+1 , j-1,n,arr,dp));
+        
+        // our choice 
+        int choice1 = arr[i]+appo1; // choose a[i]
+        int choice2 = arr[j]+appo2;  // choose a[j]
+        return dp[i][j] = max(choice1 , choice2);
         
     }
 };
+
 
 //{ Driver Code Starts.
 int main() 
