610ab1d8baa6c87f2cd91894928d4a9c7a652748
ukmaurya <ukmaurya30@gmail.com>
2024-11-22 11:17:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -3,70 +3,43 @@
 using namespace std;
 
 // } Driver Code Ends
-
-class disjointset{
-    vector<int> parent , size;
-    public: 
-      disjointset(int n ){
-          parent.resize(n);
-          size.resize(n,1);
-          for(int i=0;i<n;i++){
-              parent[i]=i;
-          }
-      }
-      int ultiPar(int node){
-          if(parent[node]==node){
-              return node;
-          }
-          return parent[node]=ultiPar(parent[node]);
-      }
-      void unionSize(int u , int v){
-          int x = ultiPar(u);
-          int y = ultiPar(v);
-          if(x==y){
-              return ;
-          }
-          if(size[x]<=size[y]){
-              parent[x] = parent[y];
-              size[y]+= size[x];
-          }
-          else{
-              parent[y] = parent[x];
-              size[x] += size[y];
-          }
-      }
-};
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-         // using krushkal algorithm 
-         disjointset ds(V+1);
-         
-         priority_queue<pair<int, pair<int,int >> , vector<pair<int, pair<int,int >> > , greater<pair<int, pair<int,int >> >>pq;
-         for(int i=0;i<V;i++){
-              for(auto it : adj[i]){
-                pq.push({it[1],{i ,it[0]}});
-               }
-         }
-         int sum=0;
-         while(!pq.empty()){
-             auto temp = pq.top();
-             pq.pop();
-             int u = temp.second.first;
-             int v = temp.second.second;
-             int cost = temp.first;
-             if(ds.ultiPar(u)!= ds.ultiPar(v)){
-                 ds.unionSize(u,v);
-                 sum += cost;
-             }
-             
-         }
-         return sum;
+        priority_queue<pair<int,int> , vector<pair<int,int>> ,greater<pair<int,int>>> minh;
+        vector<int> vis(V,0); // helps to avoid cycles
+        minh.push({0,0}); // weight, node . we are not keeping parent since we need only sum not whole MST
+        int sum=0;
+       // vis[0]=1; 
+        while(!minh.empty()){
+            auto it= minh.top();
+            minh.pop();
+            int node = it.second;
+            int wt= it.first;
+            if(vis[node]==1)
+              continue;
+            // add to the mst
+            vis[node]=1;
+            sum+=wt;
+            // now find the adjacent nodes 
+            for(auto it : adj[node]){
+                int adjnode = it[0];
+                int edgewt = it[1];
+                if(!vis[adjnode]){
+                     // vis[adjnode]=1; // it is not bfs , workout with example 
+                      minh.push({edgewt,adjnode});
+                      
+                }
+                 
+            }
+        }
+        return sum;
     }
 };
+
 
 //{ Driver Code Starts.
 
@@ -94,7 +67,9 @@ int main()
         
         Solution obj;
     	cout << obj.spanningTree(V, adj) << "\n";
-    }
+    
+cout << "~" << "\n";
+}
 
     return 0;
 }
