e115d7a1963be7d6407421416ff291f56570450f
Umesh kumar <ukmaurya30@gmail.com>
2025-02-15 16:24:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.cpp
+++ Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.cpp
@@ -2,77 +2,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<long long> printFirstNegativeInteger(long long int arr[],
-                                             long long int n, long long int k);
-
-// Driver program to test above functions
-int main() {
-    long long int t, i;
-    cin >> t;
-    while (t--) {
-        long long int n;
-        cin >> n;
-        long long int arr[n];
-        for (i = 0; i < n; i++) {
-            cin >> arr[i];
-        }
-        long long int k;
-        cin >> k;
-
-        vector<long long> ans = printFirstNegativeInteger(arr, n, k);
-        for (auto it : ans) cout << it << " ";
-        cout << endl;
-    }
-    return 0;
-}
 
 // } Driver Code Ends
 
 
+class Solution {
+  public:
+    vector<int> FirstNegativeInteger(vector<int>& arr, int k) {
+       
+       vector<int> ans;
+       int n = arr.size();
+       deque<int > dq;
+       int i=0;
+       int j=0;
+       while(j<n){
+           if(arr[j]<0){
+               dq.push_back(arr[j]);
+           }
+           if(j-i+1<k){
+               j++;
+           }
+           else if(j-i+1 == k ){
+               if(dq.empty()){
+                   ans.push_back(0);
+               }
+               else{
+                   ans.push_back(dq.front());
+                     if(arr[i]==dq.front()){
+                      dq.pop_front();
+                   }
+               }
+               // slide the window 
+             
+               i++;
+               j++;
+           }
+       }
+       
+       
+       return ans;
+       
+    }
+};
 
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
 
-vector<long long> printFirstNegativeInteger(long long int A[],
-                                             long long int N, long long int K) {
-                                                 
-                  // ideal structure                                
-                    vector<long long > ans;
-                    int i =0;
-                    int j =0;
-                    vector<long long > negatives;
-                    while(j<N){ // loop termination condition 
-                       
-                       if(A[j]< 0 ) // calculation part ie storing the negative elements 
-                        {
-                           negatives.push_back(A[j]); 
-                        }
-                        
-                        // condition part
-                        if(j-i+1<K){
-                            j++;
-                        }
-                        else if( j-i+1 == K ){ // required size
-                          // two work is done here
-                          // 1) calculation that leads to answer 
-                          // 2) slide the window 
-                          if(negatives.size()==0){ 
-                              ans.push_back(0);
-                          }
-                          else {
-                              ans.push_back(negatives.front());
-                               if(negatives.front()==A[i]){
-                                   negatives.erase(negatives.begin());
-                               }
-                          }
-                          
-                          // slide the window 
-                         
-                          i++;
-                          j++;
-                          
-                            
-                        }
-                        
-                    }
-                    return ans;
-                                                 
- }
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+        vector<int> ans = ob.FirstNegativeInteger(arr, k);
+        for (auto it : ans) {
+            cout << it << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
